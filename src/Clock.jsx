f96ba31cae5be53a6e41fee546d2d46eaa8d0e36
Robert Eisenman <robert.t.eisenman@gmail.com>
2025-12-15 16:08:03 -0800

    berserk clock

---
+++ src/Clock.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+export default function Clock() {
+    const [counter, setCounter] = useState(0);
+
+    setInterval(() => {
+        setCounter(count => count + 1)
+    }, 1000);
+
+    return (
+        <p>{counter} seconds have passed.</p>
+    );
+}
