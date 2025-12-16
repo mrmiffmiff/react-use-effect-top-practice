9f91bea38a1c98272128008d987fe8ae6cf45a36
Robert Eisenman <robert.t.eisenman@gmail.com>
2025-12-15 16:16:28 -0800

    clock no longer berserk but still wrong

--- src/Clock.jsx
+++ src/Clock.jsx
@@ -1,11 +1,13 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function Clock() {
     const [counter, setCounter] = useState(0);
 
-    setInterval(() => {
-        setCounter(count => count + 1)
-    }, 1000);
+    useEffect(() => {
+        setInterval(() => {
+            setCounter(count => count + 1)
+        }, 1000);
+    }, [])
 
     return (
         <p>{counter} seconds have passed.</p>
